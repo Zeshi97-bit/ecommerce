72ff0c3d865e9c2d2dd5e90221e6d86caebf6458
Zeshi97 <zeshishah97@gmail.com>
2021-09-21 22:29:58 -0700

    all files are uploaded

---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title Page-->
+    <title>Login</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="{{asset('admin_assets/css/font-face.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('admin_assets/vendor/font-awesome-4.7/css/font-awesome.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('admin_assets/vendor/font-awesome-5/css/fontawesome-all.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('admin_assets/vendor/mdi-font/css/material-design-iconic-font.min.css')}}" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="{{asset('admin_assets/vendor/bootstrap-4.1/bootstrap.min.css')}}" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <!--
+    <link href="{{asset('admin_assets/vendor/animsition/animsition.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('admin_assets/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('admin_assets/vendor/wow/animate.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('admin_assets/vendor/css-hamburgers/hamburgers.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('admin_assets/vendor/slick/slick.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('admin_assets/vendor/select2/select2.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('admin_assets/vendor/perfect-scrollbar/perfect-scrollbar.css')}}" rel="stylesheet" media="all">
+-->
+    <!-- Main CSS-->
+    <link href="{{asset('admin_assets/css/theme.css')}}" rel="stylesheet" media="all">
+
+</head>
+
+<body class="animsition">
+
+
+    <!-- Jquery JS-->
+    <script src="{{asset('admin_assets/vendor/jquery-3.2.1.min.js')}}"></script>
+    <!-- Bootstrap JS-->
+    <script src="{{asset('admin_assets/vendor/bootstrap-4.1/popper.min.js')}}"></script>
+    <script src="{{asset('admin_assets/vendor/bootstrap-4.1/bootstrap.min.js')}}"></script>
+    <!-- Vendor JS       -->
+    <script src="{{asset('admin_assets/vendor/slick/slick.min.js')}}">
+    </script>
+    <script src="{{asset('admin_assets/vendor/wow/wow.min.js')}}"></script>
+
+
+    <!-- Main JS-->
+    <script src="{{asset('admin_assets/js/main.js')}}"></script>
+
+</body>
+
+</html>
+<!-- end document-->
